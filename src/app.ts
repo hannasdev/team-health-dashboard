7fb59b192cbfae0196da38f1dc18e3f46aafe979
Hanna Söderström <4538260+hannasoderstromdev@users.noreply.github.com>
2024-07-30 10:13:19 +0200

    feat: add index

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 // src/app.ts
-import express from "express";
-import metricsRoutes from "./routes/metrics";
+import express from 'express';
+import metricsRoutes from './routes/metrics';
 
 const app = express();
 
 // Use the routes
-app.use("/api", metricsRoutes);
+app.use('/api', metricsRoutes);
 
 export default app;
