de067914becd88932cf11129b03879e5d07e1a2f
Hanna Söderström <4538260+hannasoderstromdev@users.noreply.github.com>
2024-08-06 13:22:50 +0200

    fix: add envs

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,5 +3,17 @@ import 'reflect-metadata';
 import { jest } from '@jest/globals';
 
 process.env.NODE_ENV = 'test';
+process.env.GOOGLE_SHEETS_ID = 'test-id';
+process.env.GOOGLE_SHEETS_CLIENT_EMAIL = 'test-user';
+process.env.GOOGLE_SHEETS_PRIVATE_KEY = 'test-key';
+process.env.GITHUB_TOKEN = 'github_pat_test';
+process.env.GITHUB_OWNER = 'test-user';
+process.env.GITHUB_REPO = 'test-org/test-repo';
+process.env.PORT = '3000';
+process.env.CORS_ORIGIN = 'http://localhost:3000,null';
+process.env.JWT_SECRET = '';
+process.env.LOG_LEVEL = 'info';
+process.env.LOG_FORMAT = 'json';
+process.env.LOG_FILE_PATH = './logs';
 
 global.jest = jest;
