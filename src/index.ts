9e68d8d8b6ee38d8e51cd59129714291b09ef81d
Hanna Söderström <4538260+hannasoderstromdev@users.noreply.github.com>
2024-08-09 16:19:46 +0200

    fix: imports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 // src/index.ts
+console.log('Current working directory:', process.cwd());
 import 'reflect-metadata';
-import './loadEnv';
-import app from './app';
+import '@/loadEnv';
+import app from '@/app';
 
 const port = process.env.PORT || 3000;
 
