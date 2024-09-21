8785aac889c06dc7eb33a147a2108fb3b8999c4e
Hanna <4538260+hannasdev@users.noreply.github.com>
2024-09-21 11:57:53 +0200

    chore: allow 5 tests to fail before bailing

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'jest';
 
 const config: Config = {
   verbose: true,
-  bail: 1,
+  bail: 5,
   preset: 'ts-jest/presets/default-esm',
   moduleFileExtensions: ['ts', 'js'],
   rootDir: './',
